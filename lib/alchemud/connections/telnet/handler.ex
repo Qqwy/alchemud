688f69478cec47a8f78ffffaf7fee703324da20e
Qqwy/Wiebe-Marten <qqwy@gmx.com>
2016-05-15 00:43:57 +0200

    It works!

--- lib/alchemud/connections/telnet/handler.ex
+++ lib/alchemud/connections/telnet/handler.ex
@@ -35,45 +35,53 @@ defmodule Alchemud.Connections.Telnet.Handler do
 
   @console_start "~> "
 
+  alias Alchemud.Connections.{Connection, HandlerBehaviour}
+
+  @behaviour HandlerBehaviour
+
   def start_link(ref, socket, transport, opts) do
-    pid = spawn_link(__MODULE__, :init, [ref, {socket, transport}, opts])
+    pid = spawn_link(__MODULE__, :init, [
+      ref, 
+      %Connection{handler_module: __MODULE__, connection_info: {socket, transport}}, 
+      opts
+    ])
     {:ok, pid}
   end
 
-  def init(ref, {socket, transport}, _opts = []) do
+  def init(ref, connection = %Connection{handler_module: __MODULE__, connection_info: {socket, transport}}, _opts = []) do
     :ok = :ranch.accept_ack(ref)
     IO.puts "New telnet connection! [socket: #{inspect socket}]"
-    send_welcome_message({socket, transport})
-    send_console_start({socket, transport})
-    loop({socket, transport})
+    send_welcome_message(connection)
+    send_console_start(connection)
+    loop(connection)
   end
 
-  def loop({socket, transport}) do
+  def loop(connection = %Connection{connection_info: {socket, transport}}) do
     case transport.recv(socket, 0, @tcp_timeout) do
       {:ok, msg} when msg in ["exit\r\n", "quit\r\n"]
         -> 
-        __MODULE__.close_connection({socket, transport})
+        __MODULE__.close_connection(connection)
       {:ok, data} ->
         IO.puts "Received data from telnet: #{inspect data}"
         formatted_data = IO.ANSI.format(["The ", :bright, "data", :normal, " is: ", :green, :bright, inspect(data)])
-        send_message({socket, transport}, formatted_data)
+        send_message(connection, formatted_data)
 
-        Alchemud.Commands.consume_command({socket, transport}, data)
+        Alchemud.Commands.consume_command(connection, data)
 
-        send_console_start({socket, transport})
-        __MODULE__.loop({socket, transport}) # Ensure that code is reloaded, if module was recompiled.
+        send_console_start(connection)
+        __MODULE__.loop(connection) # Ensure that code is reloaded, if module was recompiled.
       _ ->
-        send_message({socket, transport}, "\r\n\r\nFor safekeeping, the connection will now be closed.")
-        __MODULE__.close_connection({socket, transport})
+        send_message(connection, "\r\n\r\nFor safekeeping, the connection will now be closed.")
+        __MODULE__.close_connection(connection)
     end
   end
 
-  def send_welcome_message({socket, transport}) do
-    send_message({socket, transport}, @welcome_message)
+  def send_welcome_message(connection = %Connection{handler_module: __MODULE__}) do
+    send_message(connection, @welcome_message)
   end
 
-  def send_console_start({socket, transport}) do
-    send_message({socket, transport}, @console_start, newline: false)
+  def send_console_start(connection = %Connection{handler_module: __MODULE__}) do
+    send_message(connection, @console_start, newline: false)
   end
 
 
@@ -81,18 +89,18 @@ defmodule Alchemud.Connections.Telnet.Handler do
   Sends a message, possibly in IO.chardata-format.
   Pass newline: false to not have the message automatically end with a newline.
   """
-  def send_message({socket, transport}, message, newline: false) do
+  def send_message(%Connection{handler_module: __MODULE__, connection_info: {socket, transport}}, message, newline: false) do
     transport.send(socket, message |> IO.chardata_to_string)
     true
   end
 
-  def send_message({socket, transport}, message) do
-    send_message({socket, transport}, [message, "\r\n"], newline: false)
+  def send_message(connection = %Connection{handler_module: __MODULE__}, message) do
+    send_message(connection, [message, "\r\n"], newline: false)
   end
 
 
-  def close_connection({socket, transport}) do
-    send_message({socket, transport}, "\r\nGood bye!\r\n")
+  def close_connection(connection = %Connection{handler_module: __MODULE__, connection_info: {socket, transport}}) do
+    send_message(connection, "\r\nGood bye!\r\n")
     IO.puts "telnet connection quit! [socket: #{inspect socket}]"
     :ok = transport.close(socket)
   end
